a1ad4d35c35ac21be83173a2837acd580cd3f356
Jodelmar Beltran <jodelbeltran10@gmail.com>
2022-11-27 21:19:11 +0800

    modify quiz page

--- src/app/quiz/quiz.page.ts
+++ src/app/quiz/quiz.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quiz',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: Router
+  ) { }
 
   ngOnInit() {
   }
+
+  // goTo(page:any){
+  //   this.route.navigate([page])
+  // }
 
 }
