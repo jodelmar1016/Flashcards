48b13122df63b5756bb86a1b16904f34720171d4
Jodelmar Beltran <jodelbeltran10@gmail.com>
2022-12-20 13:44:10 +0800

    modify select subject

--- src/app/modals/select-subject/select-subject.page.ts
+++ src/app/modals/select-subject/select-subject.page.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { DataServiceService } from 'src/app/services/data-service.service';
+import { EditSubjectPage } from '../edit-subject/edit-subject.page';
 
 @Component({
   selector: 'app-select-subject',
@@ -7,11 +10,17 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./select-subject.page.scss'],
 })
 export class SelectSubjectPage implements OnInit {
-  @Input() subject: any
+  subject: any = []
 
   constructor(
     private modalCtrl: ModalController,
-  ) {}
+    private alertCtrl: AlertController,
+    private dataService: DataServiceService
+  ) {
+    this.dataService.getSubject().subscribe(res=>{
+      this.subject=res
+    })
+  }
 
   ngOnInit() {
   }
@@ -21,6 +30,43 @@ export class SelectSubjectPage implements OnInit {
       subject_Title: sub_title,
       subject_ID: sub_id
     })
+  }
+
+  async addSubject(){
+    const alert = await this.alertCtrl.create({
+      header: "Add Subject",
+      inputs: [
+        {
+          name: 'subject',
+          placeholder: 'Subject',
+          type: 'text'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Add',
+          handler: (res) => {
+            this.dataService.addSubject(res.subject)
+          }
+        }
+      ]
+    });
+    await alert.present()
+  }
+
+  async editSubject(id: string, title: string){
+    const modal = await this.modalCtrl.create({
+      component: EditSubjectPage,
+      componentProps: {id: id, title: title},
+      breakpoints: [0, 0.5],
+      initialBreakpoint: 0.5
+    });
+
+    await modal.present()
   }
 
 }
