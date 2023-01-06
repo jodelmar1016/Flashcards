175c6a43d62a8b0782d3a7aa94d61cb9d4a2a038
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-01-06 18:03:36 +0800

    remove avatar and name in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FirebaseAuthService } from './services/firebase-auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,23 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  // name: any = sessionStorage.getItem('user_display_name')
+
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: FirebaseAuthService
   ) {}
 
   logout(){
     sessionStorage.clear()
-    this.router.navigate(['/login'])
+    console.log(sessionStorage.length)
+    this.authService.signOut()
+    .then(() => {
+      console.log("User signout successfully")
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+    this.router.navigateByUrl('/login')
   }
 }
