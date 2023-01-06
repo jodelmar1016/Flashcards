1c827485b87b2b0511f00fa485251e5b62498075
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-01-06 18:02:59 +0800

    added function to update name and password

--- src/app/services/firebase-auth.service.ts
+++ src/app/services/firebase-auth.service.ts
@@ -17,4 +17,26 @@ export class FirebaseAuthService {
   signUpWithEmail(email: string, password: string){
     return this.angularFireAuth.createUserWithEmailAndPassword(email, password)
   }
+
+  // getDisplayName(){
+  //   return this.angularFireAuth.currentUser
+  // }
+
+  async getUserInfo(){
+    const user = await this.angularFireAuth.currentUser
+    return user
+  }
+
+  async updatePassword(password: string){
+    return (await this.angularFireAuth.currentUser)?.updatePassword(password)
+  }
+
+  async updateDisplayName(name: string){
+    console.log(this.angularFireAuth.currentUser)
+    return (await this.angularFireAuth.currentUser)?.updateProfile({displayName: name})
+  }
+
+  signOut(){
+    return this.angularFireAuth.signOut()
+  }
 }
