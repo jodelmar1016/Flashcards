6f2ddcb29020a5827619fc337747126f03cd5183
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-01-06 18:01:21 +0800

    working update name

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+import { FirebaseAuthService } from '../services/firebase-auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,16 +9,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  userID: any = sessionStorage.getItem('user_id')
+  userDisplayName: any
+  userEmail: any
 
   segment = "account"
 
-  constructor() { }
+  constructor(
+    private alertCtrl: AlertController,
+    private toastCtrl: ToastController,
+    private router: Router,
+    private authService: FirebaseAuthService
+  ) {
+    this.authService.getUserInfo().then(user => {
+      this.userDisplayName = user?.displayName
+      this.userEmail = user?.email
+    })
+  }
 
   ngOnInit() {
   }
 
-  onSegmentChange(event: any){
-    console.log(this.segment)
+  async updateName(){
+    const alert = await this.alertCtrl.create({
+      header: "Update Name",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Full Name',
+          type: 'text'
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel"
+        },
+        {
+          text: "Update",
+          handler: (res) => {
+            this.authService.updateDisplayName(res.name)
+            .then(() => {
+              this.userDisplayName = res.name
+              sessionStorage.setItem('user_display_name',res.name)
+              console.log(sessionStorage.getItem('user_display_name'))
+              this.presentToast("Name updated successfully!")
+            })
+          }
+        }
+      ]
+    });
+
+    await alert.present()
+  }
+
+  async presentToast(message: string){
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async updatePassword(){
+    const alert = await this.alertCtrl.create({
+      header: "Update Password",
+      inputs: [
+        {
+          name: 'password',
+          placeholder: 'New Password',
+          type: 'text',
+          max: 6
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel"
+        },
+        {
+          text: "Update",
+          handler: (res) => {
+            this.authService.updatePassword(res.password)
+          }
+        }
+      ]
+    });
+
+    await alert.present()
   }
 
 }
